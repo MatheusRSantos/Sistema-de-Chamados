cb65a370350bbefa42b1f10317e28e8f885d2b82
31550576 <31550576@099215-P31S301.macklabs.br>
2016-11-03 17:48:43 -0200

    Test

---
+++ test/Test/ChamadoDAOTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import Persistencia.ChamadoDAO;
+import entidade.Chamado;
+import entidade.RegistroChamado;
+import java.util.Collection;
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31550576
+ */
+public class ChamadoDAOTest {
+    
+    @Test
+    public void ConstructorChamadoDAO() {
+        ChamadoDAO dao = new ChamadoDAO();
+        HashMap<Integer, Chamado> cashChamado = new HashMap<>();
+        HashMap<Integer, Chamado> cashRegistroChamado = new HashMap<>();
+        assertNotNull(dao);
+    }
+
+    @Test
+    public void testGetChamados() {
+        ChamadoDAO dao = new ChamadoDAO();
+        Collection<Chamado> chamados = dao.getChamados();
+        assertNotNull(chamados);
+    }
+
+    @Test
+    public void testGetRegistrosNotNull() {
+        ChamadoDAO dao = new ChamadoDAO();
+        Collection<RegistroChamado> registros = dao.getRegistros();
+        assertNotNull(registros);
+    }
+}
